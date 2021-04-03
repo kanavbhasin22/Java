9cf7876387bbb69493af338652e0afc812796130
Kanav Bhasin <52202365+kanavbhasin22@users.noreply.github.com>
2021-04-04 02:58:27 +0530

    Delete Adding_numbers.java

--- Adding_numbers.java
+++
@@ -1,9 +0,0 @@
-package MyPackage;
-
-public class Adding_numbers {
-    public static void main(String args[]) {
-        int a = 20;
-        int b = 30;
-        System.out.println(a + b);
-    }
-}
