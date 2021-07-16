099e23f10c59a9ed4e7842698af51512417e5a66
Kanav Bhasin <52202365+kanavbhasin22@users.noreply.github.com>
2021-07-16 15:33:03 +0530

    Add files via upload

---
+++ Interface/stack.java
@@ -0,0 +1,22 @@
+package interfac;
+
+public interface stack {
+	
+	public void push(int item);
+	public int pop();
+}
+class Stack implements stack{
+	@Override
+	public void push(int item) {
+		
+	}
+	@Override
+	public int pop() {
+		
+		return 0;
+	}
+	public int top() {
+		
+		return 0;
+	}
+}
